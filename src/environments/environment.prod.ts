73412e118275f9f1b32656594da2a779952e17d6
Alex Faria <simaofaria11@gmail.com>
2018-11-26 02:45:11 +0000

    fix prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  assetsPath: "../assets/",
+  assetsPath: "../ementas/assets/",
   baseUrl: "https://portaldossas.uc.pt/PySiges/services/signetpos/get_ementas.json?Lang="
 };
